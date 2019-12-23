662bcac91afa57df33e78d1b73f67244c955eb2c
Mathieu Tarral <mathieu.tarral@protonmail.com>
2019-12-23 03:40:20 +0100

    create FDP SHM

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,22 @@
-use fdp_sys;
+use std::ffi::CString;
+
+use fdp_sys::{FDP_SHM, FDP_CreateSHM};
 
 
 #[derive(Debug)]
-pub struct FDP;
+pub struct FDP {
+    shm: *mut FDP_SHM,
+}
 
 impl FDP {
     pub fn new(vm_name: &str) -> Self {
-        FDP
+        let c_vm_name = CString::new(vm_name).unwrap();
+        // create SHM
+        let shm = unsafe {
+            FDP_CreateSHM(c_vm_name.into_raw())
+        };
+        FDP {
+            shm,
+        }
     }
 }
-
